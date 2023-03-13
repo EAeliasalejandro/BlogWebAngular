100175bb2b22b0a65ba901f80755f525f9a0ebac
Elias Alejandro <eaeliasalejandro@hotmail.com>
2023-03-13 15:16:06 -0600

    PrimerPush

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+//Esta etiqueta carga los metadatos
 @NgModule({
-  declarations: [
+  declarations: [//carga los componentes
     AppComponent
   ],
-  imports: [
+  imports: [//carga los modulos extra
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//carga los servicios extra de los componentes
+  bootstrap: [AppComponent]//se enlaza el componente principal de la aplicacion
 })
-export class AppModule { }
+export class AppModule { }//Esto es una clase de tipo modulo por la etiqueta NGModule
