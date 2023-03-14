2bff795e15a6f11ea8f6aa0c5c58a6faf80d496e
Elias Alejandro <eaeliasalejandro@hotmail.com>
2023-03-13 20:34:54 -0600

    Primeros Componentes del Blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ListadoComponent } from './views/listado/listado.component';
+import { EntradaComponent } from './views/listado/entrada/entrada.component';
 //Esta etiqueta carga los metadatos
 @NgModule({
   declarations: [//carga los componentes
-    AppComponent
+    AppComponent, MenuComponent, ListadoComponent, EntradaComponent
   ],
   imports: [//carga los modulos extra
     BrowserModule
