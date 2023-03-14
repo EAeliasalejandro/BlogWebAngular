fa0866168fd0e4db434bebe12495a7481de2827f
Elias Alejandro <eaeliasalejandro@hotmail.com>
2023-03-13 23:16:47 -0600

    Uso de DataBinding, Interfaces y Directrices

--- src/app/views/listado/entrada/entrada.component.ts
+++ src/app/views/listado/entrada/entrada.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/entrada';
 
 @Component({
   selector: 'app-entrada',
   templateUrl: './entrada.component.html',
   styleUrls: ['./entrada.component.css']
 })
-export class EntradaComponent {
+export class EntradaComponent implements OnInit{
+  @Input()
+  public entrada: Entrada;
 
+  //Constructor por defecto
+  constructor() {
+    this.entrada = {
+      titulo: '',
+      resumen: ''
+    }
+  }
+
+  ngOnInit(): void {
+
+  }
+;
 }
