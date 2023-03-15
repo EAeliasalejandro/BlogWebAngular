3100da512ce98b179cea896015592fd4bfdb7364
Elias Alejandro <eaeliasalejandro@hotmail.com>
2023-03-14 23:12:31 -0600

    Enrutado de la aplicación

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
+import { ListadoComponent } from './views/listado/listado.component';
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+//Para crear este modulo de rutas:
+//ng generate module app-routing --flat --module=app
+
+const routes: Routes  =[
+  //Cada ruta tiene que tener su dirección o nombre y el componente asociado
+  {path: 'listado',component:ListadoComponent},
+  {path: 'nosotros', component: AcercaDeNosotrosComponent},
+  //Nodo Raiz y nodo cuando no existe la ruta
+  {path: '', redirectTo: '/listado', pathMatch: 'full'},
+  {path: '**', component: PaginaNoEncontradaComponent}
+];
+
+@NgModule({
+  exports: [RouterModule],
+  //ForRoot es donde se le importan al modulo de las rutas y el nodo raiz las demas rutas
+  imports: [RouterModule.forRoot(routes)]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ListadoComponent } from './views/listado/listado.component';
 import { EntradaComponent } from './views/listado/entrada/entrada.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+import { AcercaDeNosotrosComponent } from './views/acerca-de-nosotros/acerca-de-nosotros.component';
 //Esta etiqueta carga los metadatos
 @NgModule({
   declarations: [//carga los componentes
-    AppComponent, MenuComponent, ListadoComponent, EntradaComponent
+    AppComponent, MenuComponent, ListadoComponent, EntradaComponent, PaginaNoEncontradaComponent, AcercaDeNosotrosComponent
   ],
   imports: [//carga los modulos extra
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],//carga los servicios extra de los componentes
   bootstrap: [AppComponent]//se enlaza el componente principal de la aplicacion
